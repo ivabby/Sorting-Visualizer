c2d3e8abbd830ae0517d31b3060c66897d0a1e15
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-04-05 00:04:44 +0530

    Generate Random Numbers
    Added the feature to generate numbers by sliding the slider and also added the functionality generate new array.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// var output = document.getElementById("output");
+var table = document.getElementById("myTable");
+
+size = slider.value;
+let array = []
+
+generateArrayValues();
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+    size = slider.value;
+    generateArrayValues();
+}
+
+//  Generating Random array values every time either size of slider is changed or user clicks generate new values
+function generateArrayValues(){
+    array = []
+    tableData = "<table><tr>";
+    for(let i=0;i<size;i++){
+        number = Math.floor(Math.random() * 100) + 1;
+        array.push(number);
+        tableData += "<td>" + number + "</td>";
+    }
+    tableData += "</tr></table>"
+    // output.innerHTML = array;
+    table.innerHTML = tableData;
 }
